61423b1a97010be7ccd17ae200e9e7dda8375b72
keksik77 <lukashevich7tim@gmail.com>
2020-02-25 01:15:40 +0300

    feat: completed solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+    n.split("");
+    let a = [];
+    for(let i=0;i<n.length;i++)
+    {
+        if(n[i]>=0)a.unshift(n[i]);
+    }
+    return a.join("");
 }
